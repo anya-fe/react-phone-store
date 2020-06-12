1c40166dc449a8d8b9480ebb2d4e942b857b32cf
anna <anyabt19@gmail.com>
2020-06-12 16:06:54 +0300

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+export default class Button extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export const ButtonContainer = styled.button`
+  text-transform: capitalize;
+  font-size: 1.4rem;
+  background: transparent;
+  border: 0.05rem solid var(--lightBlue);
+  border-color: ${(props) =>
+    props.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
+  color: ${(props) => (props.cart ? "var(--mainYellow)" : "var(--lightBlue)")};
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  cursor: pointer;
+  margin: 0.2rem 0.5rem 0.2rem 0;
+  transition: all 0.8 ease-in-out;
+  &:hover {
+    background: ${(props) =>
+      props.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
+    color: var(--mainBlue);
+  }
+  &:focus {
+    outline: none;
+  }
+`;
